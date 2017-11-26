0a7d0817ab9f9f24eaa3179eb71b6388180e0d47
Irvin Lim <irvinlimweiquan@gmail.com>
2017-11-26 16:40:06 +0800

    Add Array.prototype.includes test cases

---
+++ __tests__/babel/es2016-array-includes.test.js
@@ -0,0 +1,11 @@
+const { transform } = require('./__utils__');
+
+it('array literal expression', () => {
+    const code = transform('[1, 2, 3].includes(1)');
+    expect(code).toMatchSnapshot();
+});
+
+it('Array() constructor literal expression', () => {
+    const code = transform('Array(1, 2, 3).includes(1)');
+    expect(code).toMatchSnapshot();
+});
