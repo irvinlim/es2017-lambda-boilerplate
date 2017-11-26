b90095c3d302389877b19892d02c4d917b947e50
Irvin Lim <irvinlimweiquan@gmail.com>
2017-11-26 21:16:13 +0800

    Modify runner to Babel transform the input file before passing to Docker, all tests passing

--- tests/e2e/utils/runner.js
+++ tests/e2e/utils/runner.js
@@ -1,11 +1,25 @@
+const babel = require('babel-core');
+const fs = require('fs');
 const dockerLambda = require('docker-lambda');
 const path = require('path');
 
 function runTest(filename) {
-    // Construct script path within the Docker container.
-    const scriptPath = path.join('/var/task/__fixtures__/', filename);
+    // Transform the input file.
+    const readFilePath = path.join(__dirname, '../__fixtures__', filename);
+    const { code } = babel.transformFileSync(readFilePath);
 
-    // Store the result of the execution.
+    // Write the file to a temporary directory.
+    const writeFilePath = path.join(__dirname, '../__transpiled__', filename);
+    const writeFileDir = path.dirname(writeFilePath);
+    if (!fs.existsSync(writeFileDir)) {
+        fs.mkdirSync(writeFileDir);
+    }
+    fs.writeFileSync(writeFilePath, code);
+
+    // Construct the path of the transpiled script within the Docker container.
+    const dockerFilePath = path.join('/var/task/__transpiled__', filename);
+
+    // Run the Lambda function, taking in the transpiled script as the input.
     let result;
 
     try {
@@ -16,12 +30,17 @@ function runTest(filename) {
             // Specify the Lambda handler.
             handler: 'utils/handler.handler',
             // Pass an event to the Lambda function.
-            event: { EndToEndTest: { Fixture: { ScriptPath: scriptPath } } },
+            event: {
+                EndToEndTest: { Fixture: { ScriptPath: dockerFilePath } },
+            },
         });
     } catch (err) {
         console.error('Error while executing Lambda function: ', err.message);
-        return false;
+        result = false;
     }
+
+    // Clean up the transpiled file.
+    fs.unlinkSync(writeFilePath);
 
     return result === true;
 }
