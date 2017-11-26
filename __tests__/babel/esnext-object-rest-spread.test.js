f0367fe866c782ef0cfe24fd4a6f7a5ad058a3d1
Irvin Lim <irvinlimweiquan@gmail.com>
2017-11-26 17:13:29 +0800

    Add object rest/spread properties test cases

---
+++ __tests__/babel/esnext-object-rest-spread.test.js
@@ -0,0 +1,13 @@
+const { transform } = require('./__utils__');
+
+it('object spread properties', () => {
+    const code = transform('var x = { a: 1, b: 2 }; var y = { a: 3, ...x };');
+    expect(code).toMatchSnapshot();
+});
+
+it('object rest properties', () => {
+    const code = transform(
+        'var x = { a: 1, b: 2 }; var { a, ...rest } = x; console.log(rest.b);'
+    );
+    expect(code).toMatchSnapshot();
+});
